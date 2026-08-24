6622396bb537be89c8579e2325c2928ba8b1abbd
Amarberry1234 <amarberry1234@gmail.com>
2026-08-24 02:07:48 -0400

    Keep loopable duplicate and release audio playback on exit

--- ci/patch_main_audio.py
+++ ci/patch_main_audio.py
@@ -3,7 +3,7 @@ import sys
 p = Path(sys.argv[1])
 s = p.read_text()
 old = '''func _start_ambient_audio() -> void:\n    var ambience = AudioStreamPlayer.new()\n    ambience.name = "CrashSiteAmbience"\n    var stream = SFX_AMBIENT.duplicate()\n    if stream is AudioStreamWAV:\n        stream.loop_mode = AudioStreamWAV.LOOP_FORWARD\n    ambience.stream = stream\n    ambience.volume_db = -22.0\n    add_child(ambience)\n    ambience.play()\n'''
-new = '''func _start_ambient_audio() -> void:\n    var ambience = AudioStreamPlayer.new()\n    ambience.name = "CrashSiteAmbience"\n    if SFX_AMBIENT is AudioStreamWAV:\n        SFX_AMBIENT.loop_mode = AudioStreamWAV.LOOP_FORWARD\n    ambience.stream = SFX_AMBIENT\n    ambience.volume_db = -22.0\n    add_child(ambience)\n    ambience.play()\n\nfunc _exit_tree() -> void:\n    for child in get_children():\n        if child is AudioStreamPlayer:\n            child.stop()\n            child.stream = null\n'''
+new = '''func _start_ambient_audio() -> void:\n    var ambience = AudioStreamPlayer.new()\n    ambience.name = "CrashSiteAmbience"\n    var stream = SFX_AMBIENT.duplicate()\n    if stream is AudioStreamWAV:\n        stream.loop_mode = AudioStreamWAV.LOOP_FORWARD\n    ambience.stream = stream\n    ambience.volume_db = -22.0\n    add_child(ambience)\n    ambience.play()\n\nfunc _exit_tree() -> void:\n    for child in get_children():\n        if child is AudioStreamPlayer:\n            child.stop()\n            child.stream = null\n'''
 if old not in s:
     raise SystemExit('ambient audio block not found')
 p.write_text(s.replace(old, new, 1))
